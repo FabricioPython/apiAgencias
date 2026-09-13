d83b76d2d0fe7cfbf8fac86af02977c84a61c99d
opropriofabricio <fabricio.mh@outlook.com>
2026-09-12 23:10:50 -0300

    atualizacao para pagina home

--- src/app.js
+++ src/app.js
@@ -2,17 +2,15 @@ import express from "express";
 import connectToDatabases from "./config/dbconect.js";
 import routes from "./routes/index.js";
 
-const db = await connectToDatabases();
-
-db.on("error", console.log.bind(console, "Erro de conexão"));
-
-db.once("open", () => {
-    console.log("Conexão com o banco feita com sucesso!");
-});
-
-
 const app = express();
 app.use(express.static('public')); // Serve os arquivos HTML/JS locais
+
+connectToDatabases().then(db => {
+    db.on("error", console.log.bind(console, "Erro de conexão"));
+    db.once("open", () => {
+        console.log("Conexão com o banco feita com sucesso!");
+    });
+});
 routes(app);
 
 
